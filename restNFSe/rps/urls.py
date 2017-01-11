e8f7e2304dd37bac20f38446a5b8d115b9142a82
Marcelo Belli <marcelo@belli.me>
2017-01-11 16:43:40 -0200

    - App RPS: Ajustes urls

--- restNFSe/rps/urls.py
+++ restNFSe/rps/urls.py
@@ -6,8 +6,8 @@ from .views import RPSList, RPSDetail
 
 
 urlpatterns = [
-    url(r'^rps/$', RPSList.as_view()),
-    url(r'^rps/(?P<pk>[0-9]+)/$', RPSDetail.as_view()),
+    url(r'^$', RPSList.as_view()),
+    url(r'^(?P<pk>[0-9]+)/$', RPSDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
