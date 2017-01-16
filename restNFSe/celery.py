05547897bb182774d7b8334fad71b9deaf1d7a40
Marcelo Belli <marcelo@belli.me>
2017-01-15 23:03:33 -0200

    - Celery: Conf inicial

---
+++ restNFSe/celery.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restNFSe.settings')
+
+app = Celery('restNFSe')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
--- restNFSe/settings.py
+++ restNFSe/settings.py
@@ -139,3 +139,11 @@ try:
 except ImportError:
     pass
 
+# Celery Confs
+
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'America/Sao_Paulo'
