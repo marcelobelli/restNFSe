1c9aa73495e3145036202389e940eb06a20c8b0e
Marcelo Belli <marcelo@belli.me>
2017-02-09 20:05:07 -0200

    - README e LICENCE

---
+++ README.md
@@ -0,0 +1,5 @@
+# restNFSe
+Servidor REST responsável por intermediar a comunicação entre sistemas 
+de emissão de nota fiscal de serviços e o servidor SOAP da prefeitura de Curitiba.
+
+Powered by Django, Django REST Framework and PyNFSe :)
--- restNFSe/rps/comunicacao.py
+++ restNFSe/rps/comunicacao.py
@@ -1,3 +1,30 @@
 from PyNFSe.nfse.pr.curitiba import NFSeCuritiba
+from lxml import etree
 
 
+def envio_rps(rps):
+    homologacao = True if not rps.prestador.producao else False
+    nfse = NFSeCuritiba(rps.prestador.certificado_pfx.path, rps.prestador.senha_certificado,
+                        homologacao=homologacao)
+    xml_resposta = nfse.recepcionar_lote_rps(rps.to_dict())
+
+    xml = etree.fromstring(xml_resposta)
+
+    for child in xml.getchildren():
+        if child.tag == 'Protocolo':
+            return child.text
+
+    return xml_resposta
+
+# def consulta_rps_por_protocolo(prestador, protocolo):
+#     homologacao = True if not prestador.producao else False
+#     nfse = NFSeCuritiba(prestador.certificado_pfx.path, prestador.senha_certificado,
+#                         homologacao=homologacao)
+
+
+'''
+lxml
+
+for x in xml.getchildren():
+    print('{0} - {1}'.format(x.tag, x.text))
+'''
